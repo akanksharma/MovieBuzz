1a1daede4483e5bfc1554f23ad75959a8d1ecb47
Akanksha Sharma <akanksha.sharma@nw18.com>
2019-02-26 15:04:45 +0530

    Initial Commit

---
+++ MovieBuzzTests/MovieBuzzTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieBuzzTests.swift
+//  MovieBuzzTests
+//
+//  Created by Akanksha Sharma on 26/02/19.
+//  Copyright © 2019 Akanksha Sharma. All rights reserved.
+//
+
+import XCTest
+@testable import MovieBuzz
+
+class MovieBuzzTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
